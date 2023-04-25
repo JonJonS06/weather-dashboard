f96641248cc599f949868573bb06669c64f8beec
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-04-25 10:24:09 -0400

    corrected the script link in html

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,8 @@
-var btn = document.querySelector("#btn-search");
-
+let weather = {
+    "apiKey": "3e3fd5adbe0d665d9c408101d56eac67",
+    fetchWeather: function () {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=3e3fd5adbe0d665d9c408101d56eac67"
+        ).then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+}
