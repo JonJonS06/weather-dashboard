a2215b0fc4ecf155aa5e9f19c25b50d9bf229c8b
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-04-25 11:08:41 -0400

    added divs for current weather conditions

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
 let weather = {
     "apiKey": "3e3fd5adbe0d665d9c408101d56eac67",
-    fetchWeather: function () {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=3e3fd5adbe0d665d9c408101d56eac67"
-        ).then((response) => response.json())
-        .then((data) => console.log(data));
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        + "&units=imperial&appid=" 
+        + this.apiKey
+    )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
     }
 }
